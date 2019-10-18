e51da3bb788609c4aa265d9c5ab765919226f5f6
lovelykro90 <56450963+lovelykro90@users.noreply.github.com>
2019-10-18 13:39:05 -0500

    ,

--- enviarc.php
+++ enviarc.php
@@ -9,5 +9,5 @@
     $otrosserv = $_POST["otrosserv"];
     $contenido = "Nombre:".$nombre . "\nTeléfono:" . $telefono . "\nCorreo:" . $correo . "\nMensaje:" . $mensaje . "\pyme:" . $pyme . "otrosserv" . $otrosserv;
     mail($destino, "Contacto", $contenido);
-
+    header("Location:cotiza.html")
 ?>
