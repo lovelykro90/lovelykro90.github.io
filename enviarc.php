60ed0073f3438c4a4cd8bb110583f1a23dad72ed
lovelykro90 <56450963+lovelykro90@users.noreply.github.com>
2019-10-18 10:21:06 -0500

    enviar

--- enviarc.php
+++ enviarc.php
@@ -5,17 +5,9 @@
     $telefono = $_POST["Teléfono"];
     $correo = $_POST["Correo"];
     $mensaje = $_POST["Mensaje"];
-    $pyme1 = $_POST["pyme1"];
-    $pyme2 = $_POST["pyme2"];
-    $pyme3 = $_POST["pyme3"];
-    $logotipo = $_POST["logotipo"];
-    $landing = $_POST["landing"];
-    $identidad = $_POST["identidad"];
-    $web = $_POST["web"];
-    $aplicaciones = $_POST["aplicaciones"];
-    $otros = $_POST["otros"];
+    $pyme = $_POST["pyme"];
     $otrosserv = $_POST["otrosserv"];
-    $contenido = "Nombre:".$nombre . "\nTeléfono:" . $telefono . "\nCorreo:" . $correo . "\nMensaje:" . $mensaje . "\pyme1:" . $pyme1 . "\pyme2:" . $pyme2 . "\pyme3:" . $pyme3 . 
+    $contenido = "Nombre:".$nombre . "\nTeléfono:" . $telefono . "\nCorreo:" . $correo . "\nMensaje:" . $mensaje . "\pyme:" . $pyme . "otrosserv" . $otrosserv;
     mail($destino, "Contacto", $contenido);
     header("Location:cotiza.html")
 ?>
